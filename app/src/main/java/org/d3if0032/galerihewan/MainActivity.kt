cb148543d71ec565a4d62364f205a8905ae07abc
efwandha <efwandha@student.telkomuniversity.ac.id>
2021-03-08 17:33:50 +0700

    RecyclerView

--- app/src/main/java/org/d3if0032/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if0032/galerihewan/MainActivity.kt
@@ -3,11 +3,17 @@ package org.d3if0032.galerihewan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import org.d3if0032.galerihewan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         Log.d("MainActivity", "Jumlah data: "+getData().size)
     }
